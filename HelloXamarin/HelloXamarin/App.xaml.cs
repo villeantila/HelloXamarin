b83a62dd0a85604107b6e507569268f01205fd40
Ville Antila <ville.antila@stud.pointcollege.fi>
2018-08-07 18:50:18 +0300

    Numeroarvauspeli toimii.

--- HelloXamarin/HelloXamarin/App.xaml.cs
+++ HelloXamarin/HelloXamarin/App.xaml.cs
@@ -7,9 +7,30 @@ namespace HelloXamarin
 {
 	public partial class App : Application
 	{
+        private Entry syötekenttä;
+        private Label arvauksenTulosLabel;
+        private int oikeaLuku;
+
 		public App ()
 		{
 			InitializeComponent();
+
+            Random rnd = new Random();
+            oikeaLuku = rnd.Next(1, 21);
+
+            //Painonapin alustus
+            Button arvaaNappi = new Button();
+            arvaaNappi.Text = "Arvaa";
+            arvaaNappi.Clicked += ArvaaNappi_Clicked;
+
+            syötekenttä = new Entry
+            {
+                Keyboard = Keyboard.Numeric,
+                Text = ""
+            };
+
+            arvauksenTulosLabel = new Label();
+            arvauksenTulosLabel.Text = "";
 
             MainPage = new ContentPage
             {
@@ -29,13 +50,35 @@ namespace HelloXamarin
                             HorizontalTextAlignment = TextAlignment.Center,
                             Text = "Versio 0.1",
                             TextColor = Color.CadetBlue
-                        }
+                        },
+                        syötekenttä,
+                        arvaaNappi,
+                        arvauksenTulosLabel
                     }
                 }
             };
 		}
 
-		protected override void OnStart ()
+        private void ArvaaNappi_Clicked(object sender, EventArgs e)
+        {
+            int arvaus = int.Parse(syötekenttä.Text);
+            if (arvaus < oikeaLuku)
+            {
+                arvauksenTulosLabel.Text = "Luku on suurempi.";
+            }
+            else if (arvaus > oikeaLuku)
+            {
+                arvauksenTulosLabel.Text = "Luku on pienempi.";
+            }
+            else if (arvaus == oikeaLuku)
+            {
+                arvauksenTulosLabel.Text = "Arvasit oikein!";
+                Random rnd = new Random();
+                oikeaLuku = rnd.Next(1, 21);
+            }
+        }
+
+        protected override void OnStart ()
 		{
 			// Handle when your app starts
 		}
